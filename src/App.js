353c17d1680287a7473c862492c633f3a3e96646
Chiemeka Madufor <61386343+elcozy@users.noreply.github.com>
2021-10-17 01:08:58 +0100

    added responsiveness for mobile on the nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Switch, Link } from "react-router-dom";
 import "./app.scss";
 import AddRecipe from "./components/AddRecipe";
@@ -6,47 +6,72 @@ import ListRecipes from "./components/ListRepices";
 // import DeleteRecipe from "./components/DeleteRecipe";
 
 const App = () => {
+  const [isDesktop, setIsDesktop] = useState(null);
+
+  const handleWindowResize = () => {
+    const resolution = window.innerWidth;
+    const isMobile = resolution <= 768;
+    setIsDesktop(!isMobile);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize(), false);
+  }, []);
   return (
     <main className="app_container">
       <div>
-        <div
-          className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark side_nav"
-          style={{ width: 280 }}
-        >
-          <Link className="nav-link text-white" to="/">
-            <svg className="bi me-2" width="40" height="32">
-              {/* <use xlink:href="#bootstrap" /> */}
-            </svg>
-            <span className="fs-4">Recipe App</span>
-          </Link>
-          <hr />
-          <ul className="nav nav-pills flex-column mb-auto">
-            <li className="nav-item">
+        {isDesktop ? (
+          <nav
+            className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark side_nav"
+            style={{ width: 280 }}
+          >
+            <Link className="nav-link text-white" to="/">
+              <span className="fs-4">Recipe App</span>
+            </Link>
+            <hr />
+            <ul className="nav nav-pills flex-column mb-auto">
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link text-white" to="/add-recipe">
+                  Add Recipe
+                </Link>
+              </li>
+            </ul>
+            <hr />
+          </nav>
+        ) : (
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
               <Link className="nav-link text-white" to="/">
-                <svg className="bi me-2" width="16" height="16">
-                  {/* <use xlink:href="#home" /> */}
-                </svg>
-                Home
+                <span className="fs-4">Recipe App</span>
               </Link>
-            </li>
-            <li>
-              <Link className="nav-link text-white" to="/add-recipe">
-                <svg className="bi me-2" width="16" height="16">
-                  {/* <use xlink:href="#speedometer2" /> */}
-                </svg>
-                Add Recipe
-              </Link>
-            </li>
-            {/* <li>
-              <Link className="nav-link text-white" to="/list-recipes">
-                <svg className="bi me-2" width="16" height="16">
-                </svg>
-                List Recipes
-              </Link>
-            </li> */}
-          </ul>
-          <hr />
-        </div>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                  <Link className="nav-link text-white" to="/">
+                    Home
+                  </Link>
+                  <Link className="nav-link text-white" to="/add-recipe">
+                    Add Recipe
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </nav>
+        )}
       </div>
       <div className="content">
         <Switch>
